4055a50081553c6401fc72f80baa2cb17598ca34
bishank <chandbishank@gmail.com>
2020-12-09 22:08:19 -0700

    handle submit function created and array mapping on list state done

--- src/App.js
+++ src/App.js
@@ -9,7 +9,49 @@ function App() {
   const [error, setError] = useState(false)
   const [list, setList] = useState([])
 
-  return <h2>color generator project</h2>
+
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      try {
+        let colors = new Values(color).all(10)
+        setList(colors)
+      } catch (error) {
+        setError(true)
+        console.log(error)
+      }
+    }
+  
+    return (
+      <>
+        <section className='container'>
+          <h3>color generator</h3>
+          <form onSubmit={handleSubmit}>
+            <input
+              type='text'
+              value={color}
+              onChange={(e) => setColor(e.target.value)}
+              placeholder='#f15025'
+              className={`${error ? 'error' : null}`}
+            />
+            <button className='btn' type='submit'>
+              submit
+            </button>
+          </form>
+        </section>
+        <section className='colors'>
+          {list.map((color, index) => {
+            return (
+              <SingleColor
+                key={index}
+                {...color}
+                index={index}
+                hexColor={color.hex}
+              />
+            )
+          })}
+        </section>
+      </>
+  )
 }
 
 export default App
